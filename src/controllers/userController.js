713d5bd226312a2218ab98676371c85d7ea1263e
Jonathan Chasipanta <jonathan.paul065@gmail.com>
2024-07-03 11:53:15 -0500

    first commit

---
+++ src/controllers/userController.js
@@ -0,0 +1,9 @@
+exports.getUsers = (req, res) => {
+    res.json([{ id: 1, name: 'John Doe' }, { id: 2, name: 'Jane Doe' }]);
+  };
+  
+  exports.getUserById = (req, res) => {
+    const userId = req.params.id;
+    res.json({ id: userId, name: `User ${userId}` });
+  };
+  
